4474f792230b5ec261acdfe364ae8baa6c293b89
unknown <u202312217@upc.edu.pe>
2024-03-15 11:18:39 -0500

    commit s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,14 @@ export const environment = {
   production: false,
   name: 'default',
   firebase: {
-
+    config : {
+      apiKey: "AIzaSyDCJGbvwr2LApzmLf1jrS7xq4hRFmn1pfs",
+      authDomain: "ecommerce-18f25.firebaseapp.com",
+      projectId: "ecommerce-18f25",
+      storageBucket: "ecommerce-18f25.appspot.com",
+      messagingSenderId: "942365243255",
+      appId: "1:942365243255:web:c22c5c6534357556aa9372"
+    }
   },
   actionCodeSettings: {
     url: 'http://localhost:5200/profile/new',
